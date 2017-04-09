e2896d8eb77176e15723fe6ae23b6a825f95198d
yehezkel.resheff@mail.huji.ac.il <Hezi__86>
2017-04-09 11:11:43 +0300

    Adapted to tf 1.0

--- chapter2/softmax.py
+++ chapter2/softmax.py
@@ -15,7 +15,7 @@ W = tf.Variable(tf.zeros([784, 10]))
 y_true = tf.placeholder(tf.float32, [None, 10])
 y_pred = tf.matmul(x, W)
 
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_pred, y_true))
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_pred, labels=y_true))
 
 gd_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
