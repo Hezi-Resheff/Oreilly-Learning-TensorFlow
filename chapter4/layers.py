a20a0c16340fe4d9d912be9e6e0eae89793543dc
yehezkel.resheff@mail.huji.ac.il <Hezi__86>
2017-04-27 15:38:55 +0300

    Code for chapter 4 -- initial /w layers only

---
+++ chapter4/layers.py
@@ -0,0 +1,35 @@
+
+
+import tensorflow as tf
+
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                          strides=[1, 2, 2, 1], padding='SAME')
+
+
+def conv_layer(input, shape):
+    W = weight_variable(shape)
+    b = bias_variable([shape[3]])
+    return tf.nn.relu(conv2d(input, W) + b)
+
+
+def full_layer(input, size):
+    in_size = int(input.get_shape()[1])
+    W = weight_variable([in_size, size])
+    b = bias_variable([size])
+    return tf.matmul(input, W) + b
