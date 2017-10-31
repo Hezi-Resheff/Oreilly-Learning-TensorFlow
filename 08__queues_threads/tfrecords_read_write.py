07f63c2b82f311ca3e147212173e932899f46869
Lucas Moura <lucas.moura128@gmail.com>
2017-10-31 11:03:53 -0200

    Fix flake8 warnings for chapters 6, 8 and 9

--- 08__queues_threads/tfrecords_read_write.py
+++ 08__queues_threads/tfrecords_read_write.py
@@ -19,36 +19,37 @@ data_sets = mnist.read_data_sets(save_dir,
                                  dtype=tf.uint8,
                                  reshape=False,
                                  validation_size=1000)
-  
-data_splits = ["train","test","validation"]
+
+data_splits = ["train", "test", "validation"]
 for d in range(len(data_splits)):
-  print("saving " + data_splits[d])
-  data_set = data_sets[d]
+    print("saving " + data_splits[d])
+    data_set = data_sets[d]
 
-  filename = os.path.join(save_dir, data_splits[d] + '.tfrecords')
-  writer = tf.python_io.TFRecordWriter(filename)
-  for index in range(data_set.images.shape[0]):
-    image = data_set.images[index].tostring()
-    example = tf.train.Example(features=tf.train.Features(feature={
-    'height': tf.train.Feature(int64_list=
-                                  tf.train.Int64List(value=
-                                  [data_set.images.shape[1]])),
-    'width': tf.train.Feature(int64_list=
-                                  tf.train.Int64List(value =
-                                  [data_set.images.shape[2]])),
-    'depth': tf.train.Feature(int64_list=
-                                  tf.train.Int64List(value =
-                                  [data_set.images.shape[3]])),
-    'label': tf.train.Feature(int64_list=
-                                  tf.train.Int64List(value =
-                                  [int(data_set.labels[index])])),
-     'image_raw': tf.train.Feature(bytes_list=
-                                   tf.train.BytesList(value =
-                                                     [image]))}))
-    writer.write(example.SerializeToString())
-  writer.close()
+    filename = os.path.join(save_dir, data_splits[d] + '.tfrecords')
+    writer = tf.python_io.TFRecordWriter(filename)
+    for index in range(data_set.images.shape[0]):
+        image = data_set.images[index].tostring()
+        example = tf.train.Example(features=tf.train.Features(
+            feature={
+                'height': tf.train.Feature(
+                    int64_list=tf.train.Int64List(
+                        value=[data_set.images.shape[1]])),
+                'width': tf.train.Feature(
+                    int64_list=tf.train.Int64List(
+                        value=[data_set.images.shape[2]])),
+                'depth': tf.train.Feature(
+                    int64_list=tf.train.Int64List(
+                        value=[data_set.images.shape[3]])),
+                'label': tf.train.Feature(
+                    int64_list=tf.train.Int64List(
+                        value=[int(data_set.labels[index])])),
+                'image_raw': tf.train.Feature(
+                    bytes_list=tf.train.BytesList(
+                        value=[image]))}))
+        writer.write(example.SerializeToString())
+    writer.close()
 
-  
+
 filename = os.path.join(save_dir, 'train.tfrecords')
 record_iterator = tf.python_io.tf_record_iterator(filename)
 seralized_img_example = next(record_iterator)
