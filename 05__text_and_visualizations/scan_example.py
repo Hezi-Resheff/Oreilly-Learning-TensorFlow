6136f493862ed25ee7a612142044006c810fcfc8
Lucas Moura <lucas.moura128@gmail.com>
2017-10-31 10:38:29 -0200

    Fix flake8 warning for chapter 5

--- 05__text_and_visualizations/scan_example.py
+++ 05__text_and_visualizations/scan_example.py
@@ -8,8 +8,8 @@ Created on Wed Dec 28 23:20:29 2016
 import numpy as np
 import tensorflow as tf
 
-elems = np.array(["T","e","n","s","o","r", " ", "F","l","o","w"])
+elems = np.array(["T", "e", "n", "s", "o", "r",  " ",  "F", "l", "o", "w"])
 scan_sum = tf.scan(lambda a, x: a + x, elems)
 
-sess=tf.InteractiveSession()
+sess = tf.InteractiveSession()
 sess.run(scan_sum)
